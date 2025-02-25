33252dfebfb473017968c883f846976754f3317e
Rahul c shaji <rahulcshaji007@gmail.com>
2025-02-25 22:19:48 +0530

    Created Navbar and Sidebar components, added AddDoctor and Selection pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         aToken && localStorage.removeItem('aToken')
     }
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 bg-[#fff]'>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 bg-[#fff] border'>
         <div className='flex item-center gap-2 text-sm '>
             {/* <img src={assets.admin_logo} alt="" /> */}
             <p className='border text-primary px-2 py-0.5 font-bold rounded-full border-gray-500 text-gray-600 hover:bg-primary hover:text-[#fff] transition-all duration-300 cursor-pointer '>{aToken ? 'admin' : 'Doctor'}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme:{
+    extend:{
     colors:{
       'primary' : "#5F6FFF"
-    }
+    },
   },
+},
   plugins: [],
 }
 
